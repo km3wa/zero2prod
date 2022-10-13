1c1e059dc88614ba74d212ad320e3915f8461bce
kevin millot <kevin.millot@3wa.io>
2022-10-13 16:56:04 +0200

    tests: health_check.rs

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,5 @@ use zero2prod::run;
 async fn main() -> std::io::Result<()> {
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
-    run(TcpListener::bind("127.0.0.1:8000")
-        .expect("Failed to bind port 8000"))?
-        .await
+    run(TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port 8000"))?.await
 }
