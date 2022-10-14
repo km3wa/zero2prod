591af3440907271f0c0c813ae1c501c43fc615f2
kevin millot <kevin.millot@3wa.io>
2022-10-14 18:48:18 +0200

    mega commit 1

--- src/startup.rs
+++ src/startup.rs
@@ -1,10 +1,8 @@
-pub mod configuration;
-pub mod routes;
-pub mod startup;
-
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use std::net::TcpListener;
+
+use crate::routes::*;
 
 // DATABASE_URL=postgres://postgres:password@localhost:5432/newsletter
 
@@ -14,8 +12,8 @@ use std::net::TcpListener;
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
-            .route("/health_check", web::get().to(routes::health_check))
-            .route("/subscriptions", web::post().to(routes::subscribe))
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
